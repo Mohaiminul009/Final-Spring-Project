f4e4973e345166d80022b0004fd8b1a28727b1a9
Mohaiminul009 <m.moshi009@gmail.com>
2023-02-09 00:29:03 +0600

    home-09/02/23

--- src/main/java/com/spring/main/controller/CourseCategoryController.java
+++ src/main/java/com/spring/main/controller/CourseCategoryController.java
@@ -9,7 +9,6 @@ import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.repository.query.FluentQuery.FetchableFluentQuery;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
